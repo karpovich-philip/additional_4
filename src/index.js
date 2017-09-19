4f76e8def9f0acec0225d79bbaa9c8b510f64e9b
Philip <philipkarpovich@gmail.com>
2017-09-19 22:32:24 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function multiply(first, second) {
-  // your solution
+module.exports = function multiply(first, second){
+  var result = [];
+
+  for (var i = 0; i < first.length; i++) {
+    for (var j = 0; j < second.length; j++) {
+      var lenF = first.length;
+      var lenS = second.length;
+      var prevEl = (lenF - i) + (lenS - j);
+      var lastEl = prevEl + 1;
+      var mul = first[(lenF - 1) - i] * second[(lenS - 1) - j];
+      var sum = mul + (result[lastEl] || 0);
+
+      result[prevEl] = (result[prevEl]||0) + Math.floor(sum / 10); //
+      result[lastEl] = sum % 10;
+    }
+  }
+
+  result = result.slice(2);
+
+  if (result[0] === 0) {
+    result = result.slice(1);
+  }
+
+  return result.join('');
+
 }
