2d3be5e0f3f69375c26a22b3804ce558a2daf4da
Philip <philipkarpovich@gmail.com>
2017-09-25 21:53:07 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 module.exports = function multiply(first, second){
-  var result = [];
+  let result = [];
 
-  for (var i = 0; i < first.length; i++) {
-    for (var j = 0; j < second.length; j++) {
-      var lenF = first.length;
-      var lenS = second.length;
-      var prevEl = (lenF - i) + (lenS - j);
-      var lastEl = prevEl + 1;
-      var mul = first[(lenF - 1) - i] * second[(lenS - 1) - j];
-      var sum = mul + (result[lastEl] || 0);
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < second.length; j++) {
+      let lenF = first.length;
+      let lenS = second.length;
+      let prevEl = (lenF - i) + (lenS - j);
+      let lastEl = prevEl + 1;
+      let mul = first[(lenF - 1) - i] * second[(lenS - 1) - j];
+      let sum = mul + (result[lastEl] || 0);
 
-      result[prevEl] = (result[prevEl]||0) + Math.floor(sum / 10); //
+      result[prevEl] = (result[prevEl]||0) + Math.floor(sum / 10);
       result[lastEl] = sum % 10;
     }
   }
-
   result = result.slice(2);
-
   if (result[0] === 0) {
     result = result.slice(1);
   }
-
   return result.join('');
-
 }
